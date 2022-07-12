d3267af1e16da67642af5926e328b4ff6ec2d0b6
Brett Gillund <brettgillund2@gmail.com>
2022-07-12 14:23:52 -0500

    after merge with main

--- js/script.js
+++ js/script.js
@@ -1,4 +1,103 @@
+//------------------------------------------------------------create ingredients array--------------------------------------------------------------------
 
+var get_ingredients_alphabetically = "https://www.thecocktaildb.com/api/json/v2/9973533/list.php?i=list";
+var ingredients_array = [];
+var ingredients_array_object = { "A" : [], "B" : [], "C" : [], "D" : [], "E" : [], "F" : [], "G" : [], "H" : [], "I" : [], "J" : [], "K" : [], "L" : [], "M" : [], "N" : [], "O" : [], "P" : [], "Q" : [], "R" : [], "S" : [], "T" : [], "U" : [], "V" : [], "W" : [], "X" : [], "Y" : [], "Z" : [], "1" : [], "2" : [], "3" : [], "4" : [], "5" : [], "6" : [], "7" : [], "8" : [], "9" : [], "0" : [] };
+
+make_ingredients_array();
+
+$(`#console_ingredients`).on('click', function(){console.log(ingredients_array_object)});
+
+function make_ingredients_array(){
+
+    fetch(get_ingredients_alphabetically).then(function(resObject){
+        return resObject.json();
+    }).then(function(data){   
+        
+        data.drinks[239].strIngredient1 = "Lemon-lime";
+        data.drinks[240].strIngredient1 = "Lemon";
+
+        for(var i = 0; i < data.drinks.length; i++){
+
+            ingredients_array[i] = data.drinks[i].strIngredient1;
+        }        
+
+        for(var i = 0; i < ingredients_array.length; i++){
+
+           var First = ingredients_array[i][0];
+
+           ingredients_array_object[First].push(ingredients_array[i]);
+        }
+    })
+}
+//------------------------------------------------------get info based on ingredients selected------------------------------------------------------------
+
+$(`#create-cards`).on('click', get_by_ingredient);
+$(`#get_selected_drinks_info`).on('click', function(){console.log(selected_drinks_object)});
+
+var selected_drinks_ids = [];
+var selected_drinks_object = [];
+
+function get_by_ingredient(){       //calls all other functions in this section
+
+    var API_drinkIds_by_ingredients = 'https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=Vodka,Rum';
+
+    return fetch(API_drinkIds_by_ingredients).then(function(resObject){
+        return resObject.json();
+    }).then(function(data){
+
+        document.getElementById("created_card").innerHTML = `<div></div>`;
+
+        for (var i = 0; i < data.drinks.length; i++) {
+
+            selected_drinks_ids[i] = data.drinks[i].idDrink;
+
+        }
+
+        createCard(data);
+
+        get_info_by_id();
+    });
+}
+
+
+function get_info_by_id(){
+
+    var API_drink_by_ID = 'https:www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=';
+
+    for(i = 0; i < selected_drinks_ids.length; i++){
+
+        fetch(API_drink_by_ID + selected_drinks_ids[i]).then(function(resObject){ 
+            return resObject.json();
+        }).then(function(data){
+            selected_drinks_object.push(data.drinks[0])
+        })
+    }
+}
+
+
+
+function createCard(data) {     //possibly create array to store drink id to each card
+
+    for (var i = 0; i < data.drinks.length; i++) {
+        
+        document.getElementById("created_card").innerHTML += `
+            <div class="col s3">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="${data.drinks[i].strDrinkThumb}">
+                    </div>
+                    <div class="card-content">
+                        <p>Drink # ${i + 1}: ${data.drinks[i].strDrink} </p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">View Full Recipe</a>
+                    </div>
+                </div>
+            </div>`
+}}
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
 
   $(document).ready(function(){
     $('.modal').modal();
@@ -7,13 +106,12 @@
 var age_confirmed = localStorage.getItem("Age_Confirmed");    
 
 if((age_confirmed != "true") || (age_confirmed === 'undefined')){
-
     window.location.href="pages/age_verification.html";
-
 }
 
 
-//-----------------------------------------------button to reset local storeage for Age_Confirmed to test --------------------------------------------------
+//-----------------------------------------------button to reset local storeage for Age_Confirmed to test ------------------------------------------------
+
 var $btn_resetAge = $(`#age_reset`); 
 
 
@@ -27,152 +125,11 @@ $btn_resetAge.click(function(){
 
 })
 
-//-----------------------------------------------------------^^^^  TEST BUTTON   ^^^^-----------------------------------------------------------------------------------------
+//-----------------------------------------------------------^^^^  TEST BUTTON   ^^^^---------------------------------------------------------------------
 
 
 
-//-----------------------------------------------------------SIDEBAR BELOW-----------------------------------------------------------------------------------------
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    // var instances = M.Sidenav.init(elems, options);
-  });
-
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-//   var collapsibleElem = document.querySelector('.collapsible');
-//   var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    // var instances = M.Dropdown.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $('.dropdown-trigger').dropdown();
-
-//------------------------------------------------Create Cards With the drink: Name and Image ----------------------------------------------------------
-
-// //--------------getting clicked drink data---------------
-// get_by_ingredient().then(display_drink_listings);
-
-// function get_by_ingredient(){
-//     var url2 = 'https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=Vodka,Rum';
-
-//     return fetch(url2).then(function(resObject){
-//         return resObject.json();
-//     });
-
-// }
-
-// function display_drink_listings(drink_data){
-
-//     var drink_by_id = 'https:www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=';
-
-//     fetch(drink_by_id + drink_data.drinks[0].idDrink).then(function(resObject){ 
-//         return resObject.json();
-//     }).then(function(data){                  
-//         console.log(data);
-
-//     });  
-
-// }
-// //-------------------------------------------------------
-
-//var drinks_by_chosen_ingredients = [];  //practicing setting var in create card
-//get_by_ingredient();   //practicing calling in create card function
-
-
-// var drinks_by_object;
-
-// function get_by_ingredient(){
-//     var url2 = 'https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=Vodka,Rum';
-
-//     return fetch(url2).then(function(resObject){
-//         return resObject.json();
-//     }).then(function(data){   
-//         drinks_by_object = data.drinks;               
-//         console.log(data);
-//     });
-
-// }
-
-
-
-//-----------------------Populate cards using array of drinks with chosen ingredients---------
-
-// function createCard () {
-
-
-//     for (var i = 0; i < 30; i++) {
-        
-//         document.getElementById("created_card").innerHTML += `
-//         <div class="col s3">
-//         <div class="card">
-//           <div class="card-image">
-//             <img src="images/booze-background.jpg">
-//           </div>
-//           <div class="card-content">
-//             <p>Drink # ${i + 1} </p>
-//           </div>
-//           <div class="card-action">
-//             <a href="#">View Full Recipe</a>
-//           </div>
-//         </div>
-//       </div>
-        
-//         `
-// }}
-
-
-$(`#create-cards`).on('click', get_by_ingredient);
-
-function get_by_ingredient(){
-    var url2 = 'https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=Vodka,Rum';
-
-    console.log(url2);
-
-    return fetch(url2).then(function(resObject){
-        return resObject.json();
-    }).then(function(data){
-        console.log(data);
-        console.log(data.drinks);
-        console.log(data.drinks.length);
-
-
-        for (var i = 0; i < data.drinks.length; i++) {
-        
-            document.getElementById("created_card").innerHTML += `
-            <div class="col s3">
-            <div class="card">
-              <div class="card-image">
-                <img src="${data.drinks[i].strDrinkThumb}">
-              </div>
-              <div class="card-content">
-                <p>Drink # ${i + 1}: ${data.drinks[i].strDrink} </p>
-              </div>
-              <div class="card-action">
-                <a href="#">View Full Recipe</a>
-              </div>
-            </div>
-          </div>
-            
-            `
-        }
-    })
-}
-
-
-
-
-// $(`#createIngredientBtn`).on('click', create_ingredient_btns);
+//-----------------------------------------------------------SIDEBAR BELOW--------------------------------------------------------------------------------
 
 function create_ingredient_btns () {
   var ingredients = {
@@ -216,12 +173,27 @@ function create_ingredient_btns () {
       <li><a href="#!">Fourth</a></li>
     </ul></li>`
   }
-
-
-
-  
 }
 
+  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+//   var collapsibleElem = document.querySelector('.collapsible');
+//   var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    // var instances = M.Dropdown.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $('.dropdown-trigger').dropdown();
 
 
 
