b2f3be37149108a35a40b8c099dc3bb78187afec
Bradley Schill <bradschillschool@gmail.com>
2022-07-14 14:41:26 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,6 +42,7 @@ popular_homepage();
 function popular_homepage(){
 
 return fetch(API_popular_cocktails).then(function(resObject){
+    console.log(resObject);
         return resObject.json();
     }).then(function(data){
         for (var i = 0; i < data.drinks.length; i++) {
