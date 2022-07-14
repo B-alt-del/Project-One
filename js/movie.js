5753adc09bf311e76842ad6237e6f496c558a3e2
SSullivan <spencersullivan1990@gmail.com>
2022-07-13 20:06:50 -0500

    finalized movie generator still need image

--- js/movie.js
+++ js/movie.js
@@ -1,16 +1,35 @@
 var movieAPI = 'cb318d82a4b8ed6ff9428da1aaca7204';
 var random_movie_button = document.querySelector("#findRandomMovie");
-random_movie_button.addEventListener("click", function () {
 
-    movieIdEl = Math.floor(Math.random() * 1000000);
+var movieIdEl = randomIdGen();
 
-    searchMovie(movieIdEl)
 
-    function searchMovie(movieIdEl) {
-        fetch('https://api.themoviedb.org/3/movie/' + movieIdEl + '?api_key=' + movieAPI)
-            .then(response => response.json())
-            .then(data => console.log(data))
-    }
-})
+function randomIdGen() {
+    return Math.floor(Math.random() * 1000000);
+}
 
-console.log(idNumber)
+function searchMovie(movieIdEl) {
+    // console.log(movieIdEl)
+    fetch('https://api.themoviedb.org/3/movie/' + movieIdEl + '?api_key=' + movieAPI)
+        .then(response => response.json())
+        .then(data =>{ 
+            if(data.success === false) {
+                var newRandID = randomIdGen()
+                searchMovie(newRandID)    
+            }
+
+            console.log(data);
+            console.log(data.original_title)
+            document.getElementById("movieStuff").innerHTML = data.original_title;
+            
+        })
+}
+
+// random_movie_button.addEventListener("click", function () {
+
+
+//     searchMovie(movieIdEl)
+
+// })
+searchMovie(movieIdEl)
+
